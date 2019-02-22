ca1fb78a672ddbd3da9224e978fe6883df2a4749
Matt Ray <matthewhray@gmail.com>
2019-02-23 10:40:20 +1100

    remove cruft

--- test/integration/conf/conf_test.rb
+++ test/integration/conf/conf_test.rb
@@ -18,23 +18,3 @@ describe json('/tmp/inspec/inspec.json') do
   its(['reporter', 'automate', 'token']) { should eq '8ZzgdoqAPRWsW4XOHRiFx7Kbobk=' }
   its(['reporter', 'automate', 'url']) { should eq 'https://ndnd/data-collector/v0/' }
 end
-
-
-# describe json('/path/to/name.json') do
-#   its('name') { should eq 'hello' }
-#   its(['meta','creator']) { should eq 'John Doe' }
-#   its(['array', 1]) { should eq 'one' }
-# end
-
-
-#            +    "reporter" : {
-#            +        "automate" : {
-#            +            "environment" : "local",
-#            +            "insecure" : true,
-#            +            "node_name" : "conf-centos-7",
-#            +            "node_uuid": "f61e46f0-9cc1-4537-b9ee-7f29117e7d0f",
-#            +            "stdout" : "false",
-#            +            "token" : "8ZzgdoqAPRWsW4XOHRiFx7Kbobk=",
-#            +            "url" : "https://ndnd/data-collector/v0/"
-#            +        }
-#            +    }
