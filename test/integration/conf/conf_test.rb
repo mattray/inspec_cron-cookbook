91d946482e56ed53d3099bafa644415651981165
Matt Ray <matthewhray@gmail.com>
2019-09-16 23:24:57 +1200

    Updated tests to reflect mostly defaults vs. configured inspec.json
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- test/integration/conf/conf_test.rb
+++ test/integration/conf/conf_test.rb
@@ -13,7 +13,7 @@ end
 describe json('/tmp/inspec/inspec.json') do
   its(%w(reporter automate environment)) { should eq 'local' }
   its(%w(reporter automate insecure)) { should eq true }
-  its(%w(reporter automate node_name)) { should eq 'conf-centos-7' }
+  its(%w(reporter automate node_name)) { should eq 'inspec-conf-centos-7' }
   its(%w(reporter automate stdout)) { should eq false }
   its(%w(reporter automate token)) { should eq '8ZzgdoqAPRWsW4XOHRiFx7Kbobk=' }
   its(%w(reporter automate url)) { should eq 'https://automate.example.com/data-collector/v0/' }
